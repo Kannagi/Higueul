d19c3fea1a4be876cfdf3f79d0a4e004844ac938
Emmanuel <2860612-e.rousseau@users.noreply.gitlab.com>
2025-01-07 02:29:03 -0500

    fixes a small bug in the Add8

--- example/z80/out.asm
+++ example/z80/out.asm
@@ -13,34 +13,10 @@ main:
  ld sp, 0x1FFF
 ;asm
  im 1
-;NOS_state = SGA_selectedBank2 + rc
-ld d, 0
-ld e, c
-ld hl, (21)
-add hl, de
-ld a, l
-ld (17), a
-;SGA_selectedBank = NOS_state + rc
-ld l, c
-ld a, (17)
-add a, l
-ld h, 0
-ld l, a
-ld (18), hl
-;SGA_selectedBank = SGA_selectedBank2 + rc
-ld e, c
-ld hl, (20)
-add hl, de
-ld (18), hl
 ;acc = 0
 xor a
-;acc = acc + acc
-;NOS_state = idhl
-ld a, l
-ld (17), a
-;NOS_state = idhl + idhl
-add hl, hl
-ld a, l
+;NOS_state = rd + acc
+add a, d
 ld (17), a
 ;acc = rc + acc
 add a, c
@@ -52,21 +28,6 @@ ld l, c
 ld (18), hl
 ;NOS_state = rc + acc
 add a, c
-ld (17), a
-;NOS_state = rc + NOS_state
-ld l, c
-ld a, (17)
-add a, l
-ld (17), a
-;NOS_state = .ptr + rbc
-ld e, c
-ld d, b
-add (hl), de
-ld a, (hl)
-ld (17), a
-;NOS_state = .ptr + .ptr
-ld a, (hl)
-rlca
 ld (17), a
 ;asm
  ei
--- src/CPU_Z80_asm.cpp
+++ src/CPU_Z80_asm.cpp
@@ -1,3 +1,25 @@
+/*
+ * Copyright (c) 2024 Emmanuel Rousseau
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 #include <vector>
 #include <iostream>
 #include <map>
@@ -29,7 +51,7 @@
 
 // -----------------------------------------------------------------------------
 // forward declarations
-Z80OpcodeIndex build_index(void);
+static Z80OpcodeIndex build_index(void);
 
 // -----------------------------------------------------------------------------
 // global types
@@ -974,10 +996,10 @@ static std::string add8(CPU_Z80 &cpu, Z80Evaluable &src1, Z80Evaluable &src2)
 			"add " + cpu.A.to_string() + ", " + cpu.L.to_string() + "\n";
 	}
 
-	if (!cpu.A.is_undef() && cpu.A.get_value() == 0)
-	{
-		text_code = "";
-	}
+	// if (!cpu.A.is_undef() && cpu.A.get_value() == 0)
+	// {
+	// 	text_code = "";
+	// }
 
 	return text_code;
 }
